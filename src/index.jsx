9bb0b92b034fa049e2964b565b278ba7e2c57f3f
yoda <manidr@mail.ru>
2022-06-18 00:28:10 +0700

    i implemented addand reset, but it works on array

--- src/index.jsx
+++ src/index.jsx
@@ -1,57 +1,85 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import 'main.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-function AddNode(props) {
-    return (
-        <p> Node{this.props.value} </p>
-    );
-}
-
-class TreeNodes extends React.Component {
+class Panel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: [{}]
+            arrayNodes: [],
+            selectedItem: null
         };
     }
 
-    render() {
-        return (
-            <div onClick={() => {
-                console.log('Было нажатие на среднюю панель');
-            }}></div>
-        );
+    handleClick(i, action) {
+        const arrayNodes = this.state.arrayNodes.slice();
+        switch (action) {
+            case 'add':
+                arrayNodes[i] = (<p key={i} onClick={() => {
+                    this.state.selectedItem = i;
+                    console.log(this.state.selectedItem);
+                }}> Node{i} </p>);
+                break;
+            case 'remove':
+                arrayNodes = arrayNodes[i].splice();
+                break;
+            case 'edit':
+                break;
+            case 'reset':
+                arrayNodes.splice(0, arrayNodes.length);
+                break;
+        }
+        this.setState({
+            arrayNodes: arrayNodes
+        });
     }
-}
 
-class Panel extends React.Component {
+    showNodes = () => this.state.arrayNodes;
+
+    editArrayNodes(action) {
+        var lengthArr = this.state.arrayNodes.length;
+        var nextNum = lengthArr == 0 ? ++lengthArr : lengthArr; 
+        this.state.arrayNodes = this.handleClick(nextNum, action);;
+    }
+
     render() {
         return (
             <table>
-                <tr class="topRow">
-                    <th colspan="4">
-                        Tree
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                    </td>
-                </tr>
-                <tr class="underRow">
-                    <td class="underRowData">
-                        <button> Add </button>
-                    </td>
-                    <td class="underRowData">
-                        <button> Remove </button>
-                    </td>
-                    <td class="underRowData">
-                        <button> Edit </button>
-                    </td>
-                    <td class="underRowData">
-                        <button> Reset </button>
-                    </td>
-                </tr>
+                <thead>
+                    <tr className="topRow">
+                        <th colSpan={4}>
+                            Tree
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="treeData" colSpan={4}>
+                            <div className="name" onClick={() => {
+                                this.state.selectedItem = 0;
+                                console.log(this.state.selectedItem);
+                            }}>
+                                {this.showNodes()}
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr className="underRow">
+                        <td className="underRowData">
+                            <button onClick={() => this.editArrayNodes('add')}> Add </button>
+                        </td>
+                        <td className="underRowData">
+                            <button> Remove </button>
+                        </td>
+                        <td className="underRowData">
+                            <button> Edit </button>
+                        </td>
+                        <td className="underRowData">
+                            <button onClick={() => this.editArrayNodes('reset')}> Reset </button>
+                        </td>
+                    </tr>
+                </tfoot>
             </table>
         );
     }
